f1ecfaca8cc7b4a2d16923ad506dd797e3c4a61b
Manav Chadda <chadda.manav@gmail.com>
2024-09-08 02:54:46 +0530

    Space removed

--- src/main/java/com/example/demo/BookRepository.java
+++ src/main/java/com/example/demo/BookRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 // Spring Data JPA creates CRUD implementation at runtime automatically.
-
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
